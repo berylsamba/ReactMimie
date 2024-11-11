8e9fa7daa5b8f452e62227c288364cf97c046bd2
Mimie <berylsamba9@gmail.com>
2024-11-11 20:26:51 +0100

    the existig file app.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+export default function App() {
+  const name = "React Beginner";
+  const firstName = "Mimie Habib";
+  const company = "Sabilink Consulting LTD";
+
+  return (
+      <div>
+        <h1>Welcome {name}.</h1>
+        <h2>This is my first React Lesson.</h2>
+        <p>My first name is {firstName}.</p>
+        <p>I am a student of {company} doing a 5 weeks program.</p>
+        <p>I am learning Web Design using the likes of HTML, CSS and JavaScript.</p>
+        <p>My experience is going well, I am having fun learning something different from my major and trying to figure out how to tackle an issue.</p>
+        <p>My favorite is HTML and CSS. For some weird reason I find Git funny, don't know why it makes me laugh.</p>
+      </div>
+  );
+  
+}
