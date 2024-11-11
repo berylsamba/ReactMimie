e57e0539402d85bfc3bc16f07992a9332dc93ae7
Mimie <berylsamba9@gmail.com>
2024-11-11 20:32:14 +0100

    the newly modified App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ export default function App() {
 
   return (
       <div>
-        <h1>Welcome {name}.</h1>
+        <h1>Welcome{name}.</h1>
         <h2>This is my first React Lesson.</h2>
         <p>My first name is {firstName}.</p>
         <p>I am a student of {company} doing a 5 weeks program.</p>
-        <p>I am learning Web Design using the likes of HTML, CSS and JavaScript.</p>
+        <p>I was learning Problem Solving and currently learning Web Design using the likes of HTML, CSS and JavaScript.</p>
         <p>My experience is going well, I am having fun learning something different from my major and trying to figure out how to tackle an issue.</p>
         <p>My favorite is HTML and CSS. For some weird reason I find Git funny, don't know why it makes me laugh.</p>
       </div>
