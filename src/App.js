9a91da5b89c47525df35417b67d2dbab06c0fd10
Mimie <berylsamba9@gmail.com>
2024-11-12 09:05:57 +0100

    newly added changes of src/App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 export default function App() {
   const name = "React Beginner";
-  const firstName = "Mimie Habib";
+  const fullName = "Mimie Habib";
   const company = "Sabilink Consulting LTD";
 
   return (
       <div>
         <h1>Welcome {name}.</h1>
         <h2>This is my first React Lesson.</h2>
-        <p>My first name is {firstName}.</p>
+        <p>My first name is {fullName}.</p>
         <p>I am a student of {company} doing a 5 weeks program.</p>
         <p>I am learning Web Design using the likes of HTML, CSS and JavaScript.</p>
         <p>My experience is going well, I am having fun learning something different from my major and trying to figure out how to tackle an issue.</p>
